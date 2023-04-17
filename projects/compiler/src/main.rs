9ca96e5eba5716e9385eaf81801ffc8720512a39
Paul Caporn <paulcaporn@pauls-mbp.home>
2023-04-17 19:55:01 +0100

    Support multiple files

--- projects/compiler/src/main.rs
+++ projects/compiler/src/main.rs
@@ -7,14 +7,22 @@ mod vmcompiler;
 use compiler::{ CompilationEngine };
 
 fn main() {
-    let filestem = "nand2tetris/projects/11/ComplexArrays/Main";
-    let mut tokenizer = compiler::JackTokenizer::new(format!("{}.jack", filestem).as_str());
-    let mut compiler = xmlcompiler::XMLCompilationEngine::new(tokenizer.parse().unwrap().into_iter().peekable());
-    let xml = compiler.compile_class();
-    let xml_writer = xmlcompiler::XMLWriter::new(filestem);
-    xml_writer.write(&xml);
-    let mut vmcompiler = vmcompiler::VMCompiler::new(tokenizer.parse().unwrap().into_iter().peekable());
-    let vm_instructions = vmcompiler.compile_class();
-    let writer = vmcompiler::VMWriter::new( filestem );
-    writer.write(&vm_instructions);    
+    let dir = std::env::args().nth(1).unwrap();
+    let files = std::fs::read_dir(dir).unwrap();
+    for f in files {
+        let path = f.unwrap().path();
+        if path.extension().unwrap().to_str() == Some("jack") {
+            println!("{:?}", &path);
+            let filestem = format!("{}/{}", path.parent().unwrap().to_str().unwrap(), path.file_stem().unwrap().to_str().unwrap());
+            let mut tokenizer = compiler::JackTokenizer::new(path.to_str().unwrap());
+            let mut compiler = xmlcompiler::XMLCompilationEngine::new(tokenizer.parse().unwrap().into_iter().peekable());
+            let xml = compiler.compile_class();
+            let xml_writer = xmlcompiler::XMLWriter::new(filestem.as_str());
+            xml_writer.write(&xml);
+            let mut vmcompiler = vmcompiler::VMCompiler::new(tokenizer.parse().unwrap().into_iter().peekable());
+            let vm_instructions = vmcompiler.compile_class();
+            let writer = vmcompiler::VMWriter::new( filestem.as_str() );
+            writer.write(&vm_instructions);            
+        }
+    }
 }
