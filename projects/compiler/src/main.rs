a4fcb24af8cf6ddc7bfcbf7c5169bb6003800722
Paul Caporn <paulcaporn@pauls-mbp.home>
2023-04-23 09:42:42 +0100

    Tidy up

--- projects/compiler/src/main.rs
+++ projects/compiler/src/main.rs
@@ -4,7 +4,9 @@ mod vmcompiler;
 
 // use std::process::Output;
 
-use compiler::{ CompilationEngine };
+use compiler::{ CompilationEngine, JackTokenizer };
+use xmlcompiler::{ XMLCompilationEngine, XMLWriter };
+use vmcompiler::{ VMCompiler, VMWriter };
 
 fn main() {
     let dir = std::env::args().nth(1).unwrap();
@@ -14,14 +16,16 @@ fn main() {
         if path.extension().unwrap().to_str() == Some("jack") {
             println!("{:?}", &path);
             let filestem = format!("{}/{}", path.parent().unwrap().to_str().unwrap(), path.file_stem().unwrap().to_str().unwrap());
-            let mut tokenizer = compiler::JackTokenizer::new(path.to_str().unwrap());
-            let mut compiler = xmlcompiler::XMLCompilationEngine::new(tokenizer.parse().unwrap().into_iter().peekable());
-            let xml = compiler.compile_class();
-            let xml_writer = xmlcompiler::XMLWriter::new(filestem.as_str());
+            let mut tokenizer = JackTokenizer::new(path.to_str().unwrap());
+            
+            let mut xmlcompiler = XMLCompilationEngine::new(tokenizer.parse().unwrap().into_iter().peekable());
+            let xml = xmlcompiler.compile_class();
+            let xml_writer = XMLWriter::new(filestem.as_str());
             xml_writer.write(&xml);
-            let mut vmcompiler = vmcompiler::VMCompiler::new(tokenizer.parse().unwrap().into_iter().peekable());
+            
+            let mut vmcompiler = VMCompiler::new(tokenizer.parse().unwrap().into_iter().peekable());
             let vm_instructions = vmcompiler.compile_class();
-            let writer = vmcompiler::VMWriter::new( filestem.as_str() );
+            let writer = VMWriter::new( filestem.as_str() );
             writer.write(&vm_instructions);            
         }
     }
