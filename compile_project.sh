d56299d1ecb273ee9b507f726866add17afc5ba9
Paul Caporn <paulcaporn@pauls-mbp.home>
2023-05-01 20:10:57 +0100

    merge

---
+++ compile_project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# clean up build directory
+if [ -d "build" ] 
+then
+    rm -rf build/*
+else 
+    mkdir build
+fi
+
+# copy OS files in build dir
+cp nand2tetris/tools/OS/*.vm build
+# copy any pre-compiled vm files from target into the build folder
+cp $1/* build
+# compile jack files into vm files in the build directory
+cargo run --bin compiler $1 build
+# compile vm files in build file into ASM and HACK files
+cargo run --bin vmtranslator build
