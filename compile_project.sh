40ba9ff3c2631c6a9daeef804ff6489f9961a25e
Paul Caporn <paulcaporn@pauls-mbp.home>
2023-05-01 21:07:39 +0100

    fix comment

--- compile_project.sh
+++ compile_project.sh
@@ -10,11 +10,9 @@ fi
 
 # copy OS files in build dir
 #cp nand2tetris/tools/OS/*.vm build
-# copy any pre-compiled vm files from target into the build folder
+# copy files from target into the build folder
 cp $1/* build
 target=`basename $1`
-
-echo $target
 # compile jack files into vm files in the build directory
 cargo run --bin compiler $1 build
 # compile vm files in build file into ASM and HACK files
